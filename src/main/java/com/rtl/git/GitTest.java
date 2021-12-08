c441b5d925a3d19cdc2ad332d174ea17499348a2
raotenglong <raotenglong@qq.com>
2021-12-09 01:34:09 +0800

    first commit

---
+++ src/main/java/com/rtl/git/GitTest.java
@@ -0,0 +1,8 @@
+package com.rtl.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
