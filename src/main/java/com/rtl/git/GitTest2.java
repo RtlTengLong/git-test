f35532c5b10873740b17f131be25fcc1cd8c5937
raotenglong <raotenglong@qq.com>
2021-12-09 02:48:49 +0800

    master commit2

--- src/main/java/com/rtl/git/GitTest2.java
+++ src/main/java/com/rtl/git/GitTest2.java
@@ -9,5 +9,6 @@ public class GitTest2 {
         System.out.println("3333");
         System.out.println("4444");
         System.out.println("4444");
+        System.out.println("master test");
     }
 }
