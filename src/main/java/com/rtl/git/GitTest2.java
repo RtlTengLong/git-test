a687b94e85c328516c0f1487e7c3122f68d0d0ad
raotenglong <raotenglong@qq.com>
2021-12-09 01:38:29 +0800

    second commit

--- src/main/java/com/rtl/git/GitTest2.java
+++ src/main/java/com/rtl/git/GitTest2.java
@@ -3,5 +3,7 @@ package com.rtl.git;
 public class GitTest2 {
     public static void main(String[] args) {
         System.out.println("2222");
+        System.out.println("2222");
+        System.out.println("2222");
     }
 }
